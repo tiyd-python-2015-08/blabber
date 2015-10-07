ce1fdce3cd44dce3f072a30aacc27e892356b59a
James M. Allen <james.m.allen@gmail.com>
2015-10-07 12:20:35 -0400

    Added user views

--- blabber/blabber/urls.py
+++ blabber/blabber/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^statuses/all', views.all_statuses),
     url(r'^statuses/', views.recent_statuses),
+    url(r'^user/(?P<user_id>\d+)$', views.show_user),
+    url(r'^user/(?P<username>\S+)$', views.show_user_by_username),
 ]
--- blabber/updates/views.py
+++ blabber/updates/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Status
+from .models import Status, User
 
 # Create your views here.
 
@@ -13,8 +13,23 @@ def all_statuses(request):
 
 def recent_statuses(request):
     statuses = Status.objects.order_by('-posted_at')[:20]
-    status_strings = [str(status) for status in statuses]
+    # status_strings = [str(status) for status in statuses]
     # return HttpResponse('<br>'.join(status_strings))
     return render(request,
                   'updates/statuses.html',
-                  {'statuses': status_strings})
+                  {'statuses': statuses})
+
+
+def show_user(request, user_id):
+    user = User.objects.get(pk=user_id)
+    statuses = user.status_set.all().order_by('-posted_at')
+
+    return render(request,
+                  'updates/user.html',
+                  {'user': user,
+                   'statuses': statuses})
+
+
+def show_user_by_username(request, username):
+    user_id = User.objects.get(username=username).id
+    return show_user(request, user_id=user_id)
