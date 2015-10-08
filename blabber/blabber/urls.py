84b1680456c80c27f96592448e0157c9b3d5a4bc
James M. Allen <james.m.allen@gmail.com>
2015-10-08 11:39:00 -0400

    making urls shorter

--- blabber/blabber/urls.py
+++ blabber/blabber/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^u/', include('updates.urls'))
+    url(r'^', include('updates.urls'))
 ]
