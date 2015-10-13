e539f8df09a586b5b6add28762d1a0560639159e
James M. Allen <james.m.allen@gmail.com>
2015-10-13 11:39:04 -0400

    Switch to postgres

--- blabber/blabber/settings.py
+++ blabber/blabber/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'blabber.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blabber',
+        'USER': 'blabber',
+        'PASSWORD': 'blabber',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
--- blabber/updates/views.py
+++ blabber/updates/views.py
@@ -18,7 +18,7 @@ def all_statuses(request):
 
 
 def recent_statuses(request):
-    if request.user:
+    if request.user.is_authenticated():
         statuses = Status.objects.filter(
             user__profile__in=request.user.profile.following.all())
         statuses = statuses.order_by('-posted_at')[:20]
@@ -26,6 +26,10 @@ def recent_statuses(request):
         statuses = Status.objects.order_by('-posted_at')[:20]
     # status_strings = [str(status) for status in statuses]
     # return HttpResponse('<br>'.join(status_strings))
+
+    # statuses = statuses.select_related('user')
+    statuses = statuses.prefetch_related('user')
+
     return render(request,
                   'updates/statuses.html',
                   {'statuses': statuses})
